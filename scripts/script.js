b4b565f95fa8f47195bddec7a504b244e81d7fcf
Leoniqi <leo.vuniqi55@gmail.com>
2018-04-05 22:40:00 -0400

    HTML,Script,Style Edits
    --Leo--
    Edited the script.js file to work with the different add to cart buttons adding the different items to the table
    
    Edited the style.css file to work with the seperate buttons
    
    Edited the index.html file to work with the changes made with the javascript and css files, changing some names on the buttons and the input fields

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
-$('#btnAddToCart').click(function(){
-    $('.myTable').append('<tr><td>my data</td><td>more data</td></tr>');
+var appendHP = "<tr><td>Harry Potter and the Order of the Phoenix</td><td></td><td>$12.99</td></tr>";
+var appendWT = "<tr><td>Wheel of Time #1: The Eye of the World</td><td>1</td><td>$9.99</td></tr>";
+var appendP1 = "<tr><td>Ready Player One</td><td>1</td><td>$15.99</td></tr>";
+$('#btnAddToCartHP').click(function(){
+    $('#myTable').append(appendHP);
+    console.log("appended");
+});
+
+$('#btnAddToCartWT').click(function(){
+    $('#myTable').append(appendWT);
+    console.log("appended");
+});
+
+$('#btnAddToCartP1').click(function(){
+    $('#myTable').append(appendP1);
     console.log("appended");
 });
 
