07b184c8ef557c693b906a05c07aa06550082230
Leoniqi <leo.vuniqi55@gmail.com>
2018-04-07 23:21:16 -0400

    value of order quantity
    got the value of the order quantity to display on the table and stored in a variable

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
-var appendHP = "<tr><td>Harry Potter and the Order of the Phoenix</td><td></td><td>$12.99</td></tr>";
+//var appendHP = "<tr><td>Harry Potter and the Order of the Phoenix</td><td></td><td>$12.99</td></tr>";
 var appendWT = "<tr><td>Wheel of Time #1: The Eye of the World</td><td>1</td><td>$9.99</td></tr>";
 var appendP1 = "<tr><td>Ready Player One</td><td>1</td><td>$15.99</td></tr>";
 $('#btnAddToCartHP').click(function(){
-    $('#myTable').append(appendHP);
+    var quantityHP = $('#orderQuantityHP').val();
+    $('#myTable').append("<tr><td>Harry Potter and the Order of the Phoenix</td><td>"+quantityHP+"</td><td>$12.99</td></tr>");
     console.log("appended");
+
 });
 
 $('#btnAddToCartWT').click(function(){
-    $('#myTable').append(appendWT);
+    var quantityWT = $('#orderQuantityWT').val();
+    $('#myTable').append("<tr><td>Wheel of Time #1: The Eye of the World</td><td>"+quantityWT+"</td><td>$9.99</td></tr>");
     console.log("appended");
 });
 
 $('#btnAddToCartP1').click(function(){
-    $('#myTable').append(appendP1);
+    var quantityP1 = $('#orderQuantityP1').val();
+    $('#myTable').append("<tr><td>Ready Player One</td><td>"+quantityP1+"</td><td>$15.99</td></tr>");
     console.log("appended");
 });
 
