f5d277376dcab50cb619382f45cb0fddb681fc5b
Leoniqi <leo.vuniqi55@gmail.com>
2018-04-07 23:46:08 -0400

    blank

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 //var appendHP = "<tr><td>Harry Potter and the Order of the Phoenix</td><td></td><td>$12.99</td></tr>";
-var appendWT = "<tr><td>Wheel of Time #1: The Eye of the World</td><td>1</td><td>$9.99</td></tr>";
-var appendP1 = "<tr><td>Ready Player One</td><td>1</td><td>$15.99</td></tr>";
+//var appendWT = "<tr><td>Wheel of Time #1: The Eye of the World</td><td>1</td><td>$9.99</td></tr>";
+//var appendP1 = "<tr><td>Ready Player One</td><td>1</td><td>$15.99</td></tr>";
 $('#btnAddToCartHP').click(function(){
     var quantityHP = $('#orderQuantityHP').val();
     $('#myTable').append("<tr><td>Harry Potter and the Order of the Phoenix</td><td>"+quantityHP+"</td><td>$12.99</td></tr>");
     console.log("appended");
-
 });
 
 $('#btnAddToCartWT').click(function(){
